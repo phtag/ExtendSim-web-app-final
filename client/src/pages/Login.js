b2f74cd8446f6ad1d409cbfbd4a29d5f5d85db66
Peter Tag <peter.h.tag@gmail.com>
2019-05-23 09:29:47 -0700

    Migrating to userContext sharing of state data

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -34,33 +34,71 @@ class Login extends React.Component {
     return (
       <UserContext.Consumer>
         {({handleUserInputChange, handleLoginSubmit, username, password, validationObjects}) => (
-          <div>
-            <h1>Login</h1>
-            <label htmlFor="name">Username</label>
-            <input
-              autoComplete="off"
-              type="text"
-              name="username"
-              value={username}
-              onChange={(e) => this.handleChange(e, 'username', handleUserInputChange)}
-            />
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              name="password"
-              value={password}
-              onChange={(e) => this.handleChange(e, 'password', handleUserInputChange)}
-            />
+        //   <div>
+        //     <h1>Login</h1>
+        //     <label htmlFor="name">Username</label>
+        //     <input
+        //       autoComplete="off"
+        //       type="text"
+        //       name="username"
+        //       value={username}
+        //       onChange={(e) => this.handleChange(e, 'username', handleUserInputChange)}
+        //     />
+        //     <label htmlFor="password">Password</label>
+        //     <input
+        //       type="password"
+        //       name="password"
+        //       value={password}
+        //       onChange={(e) => this.handleChange(e, 'password', handleUserInputChange)}
+        //     />
 
-            <button onClick={() => this.handleLogin(handleLoginSubmit)}>Sign up</button>
-            <br />
-            { error && (
-              <div className="alert">
-                {error}
-              </div>
-            )}
-            <pre>{JSON.stringify(this.state, null, 2)}</pre>
-          </div>
+        //     <button onClick={() => this.handleLogin(handleLoginSubmit)}>Sign up</button>
+        //     <br />
+        //     { error && (
+        //       <div className="alert">
+        //         {error}
+        //       </div>
+        //     )}
+        //     <pre>{JSON.stringify(this.state, null, 2)}</pre>
+        //   </div>
+        <div id="home">
+        <div className="container">
+            <div className="row">
+                <header id="ExtendSim-header">
+                </header>
+            </div>
+            <div className="row">
+                <div className="col-8 offset-2">
+                    <h2>ExtendSim Web Simulation Login</h2>
+                    <form className="clearfix mb-4" action="POST">
+                        <div className="form-group">
+                            <label htmlFor="example-text">Username</label>
+                            <input 
+                            onChange={(e) => this.handleChange(e, 'username', handleUserInputChange)}
+                            type="text" id="username-text" 
+                            className="form-control" 
+                            aria-describedby="example-text" 
+                            placeholder="Enter username"></input>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="password-text">Password</label>
+                            <input 
+                            onChange={(e) => this.handleChange(e, 'username', handleUserInputChange)}
+                            type="password" 
+                            id="password-text" 
+                            className="form-control" 
+                            aria-describedby="password-text"></input>
+                        </div>
+                        <button 
+                            onClick={() => this.handleLogin(handleLoginSubmit)}
+                            disabled={!validationObjects[validationObjects.findIndex(obj => obj.name==="loginSubmitButton")].enabled}
+                            id="submit-login-info" className="btn btn-primary float-left">Submit
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
         )}
       </UserContext.Consumer>
     );
